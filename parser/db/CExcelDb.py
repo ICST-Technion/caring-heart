06275e50496615959608a2cfeb26314538e1a18a
Levi, George <george.levi@intel.com>
2021-11-25 02:19:47 +0200

    add excel database handler empty placeholder

---
+++ parser/db/CExcelDb.py
@@ -0,0 +1,22 @@
+from parser.interfaces.IDb import IDb
+
+
+class CExcelDb(IDb):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def connect(self, *args, **kwargs):
+        pass
+
+    def close(self):
+        pass
+
+    def insert(self, *args, **kwargs):
+        pass
+
+    def update(self, *args, **kwargs):
+        pass
+
+    def get(self, *args, **kwargs):
+        pass
