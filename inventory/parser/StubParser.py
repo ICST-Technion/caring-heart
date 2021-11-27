a924374b8aa1ad3978a3ab40f230b7209e982898
Levi, George <george.levi@intel.com>
2021-11-27 17:46:54 +0200

    stub parser initial commit

---
+++ inventory/parser/StubParser.py
@@ -0,0 +1,8 @@
+from inventory.interfaces.IParser import IParser
+
+
+class Parser(IParser):
+
+    def parse(self, text):
+        columns = ["Name", "Address", "Street", "City", "Phone", "Description", "Category", "Date", "Comments", "Email"]
+        return {columns[index]: item.strip() for index, item in enumerate( text.split(","))}
