a7e6f91906b03d14d78eb3340151cf03d9fbebc9
Levi, George <george.levi@intel.com>
2021-11-27 17:54:54 +0200

    Inventory initial commit

---
+++ inventory/Inventory.py
@@ -0,0 +1,10 @@
+"""Region IDE imports"""
+from interfaces.IDb import IDb
+
+
+class Inventory(object):
+    def __init__(self, db: IDb):
+        self._db = db
+
+    def add_to_inventory(self, item: {}):
+        self._db.insert(item=item)
