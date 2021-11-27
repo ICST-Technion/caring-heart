5141d3109428834ebe69be2c3d58aa8c74f74860
Levi, George <george.levi@intel.com>
2021-11-27 17:53:06 +0200

    aligned file name a class name to project's naming conventions

--- inventory/factories/DbFactory.py
+++ inventory/factories/DbFactory.py
@@ -7,7 +7,7 @@ class DbProvider(object):
         if db is None or db.lower() == "stub":
             return None
         elif db.lower() == "excel":
-            from inventory.db.CExcelDb import CExcelDb as database
+            from inventory.db.ExcelDb import ExcelDb as database
         else:
             raise Exception(f"Unknown db {db}")
         return database(*args, **kwargs)
