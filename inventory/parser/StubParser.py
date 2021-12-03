72c3cd9a1650b7e46e5fd848b0502b8b24b895f4
George <george36be@gmail.com>
2021-12-03 11:27:53 +0200

    add stub flow to test gmail api

--- inventory/parser/StubParser.py
+++ inventory/parser/StubParser.py
@@ -5,4 +5,4 @@ class Parser(IParser):
 
     def parse(self, text):
         columns = ["Name", "Address", "Street", "City", "Phone", "Description", "Category", "Date", "Comments", "Email"]
-        return {columns[index]: item.strip() for index, item in enumerate( text.split(","))}
+        return {columns[index]: item.strip() for index, item in enumerate( text.split('\n')[0].split(","))}
