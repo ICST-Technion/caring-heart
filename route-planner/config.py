4f3bd0a6f03782eccb6c9b275b873f98a6838a38
Dor-B <dor46200@gmail.com>
2021-12-05 11:19:32 +0200

    Change config GUI to easily support more variables and sections with the ConfigVar dataclass

--- route-planner/config.py
+++ route-planner/config.py
@@ -1,67 +1,114 @@
+import dataclasses
+from dataclasses import dataclass
+from typing import Union, List
+
 import PySimpleGUI as sg
 import string
 from configparser import ConfigParser
 import os
 
-AtoZ = string.ascii_uppercase
+AtoZ = list(string.ascii_uppercase)
 NO_COLUMN = 'התעלם, הכנס הכל'
 CONFIG_FILE = 'config.ini'
 
 
-def write_config(config):
-    with open(CONFIG_FILE, 'w') as config_file:
-        config.write(config_file)
+@dataclass
+class ConfigVar:
+    name: str
+    text: str
+    default_value: Union[str, int]
+    section: str
 
 
-def column_to_index(column_letter: str):
-    if column_letter not in AtoZ:
-        return -1
-    return AtoZ.index(column_letter)
+@dataclass
+class ColumnVarType(ConfigVar):
+    default_value: str
+    no_column_option: bool = False
 
 
-def index_to_column(column_index: int):
-    if column_index < 0:
-        return NO_COLUMN
-    return AtoZ[column_index]
+@dataclass
+class FileVarType(ConfigVar):
+    default_value: str
+
+
+@dataclass
+class NumberRangeVarType(ConfigVar):
+    default_value: int
+    start: int
+    end: int
+
+
+def config_var_ui(config_var: ConfigVar):
+    def key_and_default(is_text):
+        return {'key': config_var.name, ('default_text' if is_text else 'default_value'): config_var.default_value}
+
+    text_element = sg.Text(config_var.text, justification="right")
+    if type(config_var) == FileVarType:
+        return [
+            sg.In(size=(25, 1), enable_events=True, **key_and_default(is_text=True)),
+            sg.FileBrowse(),
+            text_element
+        ]
+    elif type(config_var) == ColumnVarType:
+        no_column = [NO_COLUMN] if config_var.no_column_option else []
+        return [
+            sg.Combo(no_column + AtoZ, **key_and_default(is_text=False)),
+            text_element
+        ]
+    elif type(config_var) == NumberRangeVarType:
+        return [
+            sg.Combo(list(range(config_var.start, config_var.end)), **key_and_default(is_text=False)),
+            text_element
+        ]
+
+
+class MyConfig:
+    def __init__(self, configs_vars: List[ConfigVar], path=CONFIG_FILE):
+        self.config = ConfigParser()
+        self.path = path
+        self.config.read(path)
+
+        if not os.path.exists(path):
+            sections = set((var.section for var in configs_vars))
+            for section in sections:
+                self.config[section] = {config_var.name: config_var.default_value for config_var in configs_vars if config_var.section == section}
+            self._write_config()
+            self._config_vars = configs_vars
+            return
+
+        self._config_vars = []
+        for config_var in configs_vars:
+            val = self.config.get(config_var.section, config_var.name)
+            new_config = dataclasses.replace(config_var, default_value=val)
+            self._config_vars.append(new_config)
+
+    def write_changes(self, values_dict):
+        for var in config_vars:
+            self.config.set(var.section, var.name, str(values_dict[var.name]))
+        self._write_config()
+
+    def _write_config(self):
+        with open(self.path, 'w') as config_file:
+            self.config.write(config_file)
 
 
 if __name__ == '__main__':
-    config = ConfigParser()
-    config.read(CONFIG_FILE)
+    columns = ((name, 'עמודת ' + text, col) for col, (name, text) in zip(AtoZ, (
+        ('name', 'שם'), ('address', 'כתובת'), ('city', 'עיר'), ('phone', 'טלפון'), ('description', 'תיאור'),
+        ('category', 'קטגוריה'), ('date', 'תאריך'), ('comments', 'הערות')
+    )))
+    config_vars = (
+        FileVarType('excel_path', 'קובץ האקסל', 'template.xlsx', section='main'),
+        *map(lambda args: ColumnVarType(*args, section='columns'), columns),
+        ColumnVarType('selection', "עמודה לסימון פריטים לתכנון מסלול", 'K', no_column_option=True, section='main'),
+        NumberRangeVarType('max_show', "מספר מקסימלי של פריטים לתצוגה", default_value=8, start=3, end=20, section='main'),
+    )
 
-    if not os.path.exists(CONFIG_FILE):
-        config['main'] = {'excel_path': 'template.xlsx', 'address_column': 1, 'selection_column': 10, 'max_show': 8}
-        write_config(config)
-
-    excel_path = config.get('main', 'excel_path')
-    address_column = config.getint('main', 'address_column')
-    selection_column = config.getint('main', 'selection_column')
-    max_show = config.getint('main', 'max_show')
-
-    excel_path_chooser = [
-            sg.In(size=(25, 1), enable_events=True, key="excel_path", default_text=excel_path),
-            sg.FolderBrowse(),
-            sg.Text("בחר קובץ אקסל", justification="right"),
-    ]
-    address_column_chooser = [
-            sg.Combo([*AtoZ], key='address_column', default_value=index_to_column(address_column)),
-            sg.Text("עמודת הכתובת", justification="right"),
-    ]
-    selected_column_chooser = [
-            sg.Combo([NO_COLUMN, *AtoZ], key='selection_column', default_value=index_to_column(selection_column)),
-            sg.Text("עמודה לסימון פריטים לתכנון מסלול", justification="right"),
-    ]
-    max_show_chooser = [
-            sg.Combo(list(range(3, 30)), key='max_show', default_value=max_show),
-            sg.Text("מספר מקסימלי של פריטים להראות", justification="right"),
-    ]
+    my_config = MyConfig(config_vars)
 
     layout = [
         [sg.Text("הגדרות")],
-        excel_path_chooser,
-        address_column_chooser,
-        selected_column_chooser,
-        max_show_chooser,
+        *map(config_var_ui, config_vars),
         [sg.Button("שמור")],
     ]
 
@@ -76,10 +123,6 @@ if __name__ == '__main__':
         if event == sg.WIN_CLOSED:
             break
         if event == "שמור":
-            config.set('main', 'excel_path', values['excel_path'])
-            config.set('main', 'address_column', str(column_to_index(values['address_column'])))
-            config.set('main', 'selection_column', str(column_to_index(values['selection_column'])))
-            config.set('main', 'max_show', str(values['max_show']))
-            write_config(config)
+            my_config.write_changes(values)
             break
     window.close()
