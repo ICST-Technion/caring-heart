8451f423e41bb7ce4e54463029a5f87e52647d24
George <george36be@gmail.com>
2021-12-15 00:27:04 +0200

    Excel DB merge

--- inventory/db/DateTimeHelper.py
+++ inventory/db/DateTimeHelper.py
@@ -1,19 +1,19 @@
 from datetime import datetime
-import locale
+#import locale
 
 
-#MONTHS = ['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר',
-#          'דצמבר']
+MONTHS = ['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני', 'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר',
+          'דצמבר']
 
-
-#def sheetname_getter():
-#    month = datetime.now().month
-#    year = datetime.now().year
-#    return f'{MONTHS[month - 1]} {year}'
 
 def sheetname_getter():
-    locale.setlocale(locale.LC_ALL, 'he_IL')
-    d = datetime.now()
-    month_year = d.strftime('%B %Y')
-    locale.resetlocale(locale.LC_ALL)
-    return month_year
+    month = datetime.now().month
+    year = datetime.now().year
+    return f'{MONTHS[month - 1]} {year}'
+
+#def sheetname_getter():
+#    locale.setlocale(locale.LC_ALL, 'he_IL')
+#    d = datetime.now()
+#    month_year = d.strftime('%B %Y')
+#    locale.resetlocale(locale.LC_ALL)
+#    return month_year
