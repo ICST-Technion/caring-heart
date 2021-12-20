a58164cff9d54f408468c5c0191b043afa329ff5
Daniel Lieberman <daniel.lie@campus.technion.ac.il>
2021-12-20 17:25:27 +0200

    Added Item List Display with on\off switches

--- route_planner_ui/lib/item.dart
+++ route_planner_ui/lib/item.dart
@@ -1,5 +1,6 @@
 class Item {
   final String address;
+  final String neighborhood;
   final String category;
   final String city;
   final String comments;
@@ -12,24 +13,25 @@ class Item {
   final String id;
   final String phone;
 
-  Item(
-      {required this.address,
-      required this.category,
-      required this.city,
-      required this.comments,
-      required this.date,
-      required this.description,
-      required this.email,
-      required this.isChecked,
-      required this.isCollected,
-      required this.name,
-      required this.id,
-      required this.phone});
+  Item({required this.address,
+    required this.neighborhood,
+    required this.category,
+    required this.city,
+    required this.comments,
+    required this.date,
+    required this.description,
+    required this.email,
+    required this.isChecked,
+    required this.isCollected,
+    required this.name,
+    required this.id,
+    required this.phone});
 
   factory Item.fromJson(String id, Map<String, dynamic> json) {
     return Item(
         id: id,
         address: json['address'],
+        neighborhood: json['neighborhood'],
         category: json['category'],
         city: json['city'],
         comments: json['comments'],
--- route_planner_ui/lib/main.dart
+++ route_planner_ui/lib/main.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:route_planner_ui/item_list_provider.dart';
+import 'package:tuple/tuple.dart';
 import 'item_service.dart' as DB;
 import 'item.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -58,7 +64,7 @@ class App extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             body:
-            Center(child: Text(error, textDirection: TextDirection.ltr))));
+                Center(child: Text(error, textDirection: TextDirection.ltr))));
   }
 }
 
@@ -77,13 +83,18 @@ class MyApp extends StatelessWidget {
             return ErrorScreen(snapshot.error.toString());
           }
           if (snapshot.connectionState == ConnectionState.done) {
-            return MaterialApp(
-              title: 'תכנון מסלול',
-              theme: ThemeData(
-                primarySwatch: Colors.pink,
+            return ChangeNotifierProvider(
+              create: (_) => ItemListProvider(
+                  list: snapshot.data!.map((e) => Tuple2(false, e)).toList()),
+              child: MaterialApp(
+                title: 'תכנון מסלול',
+                theme: ThemeData(
+                  primarySwatch: Colors.pink,
+                ),
+                debugShowCheckedModeBanner: false,
+                home:
+                    MyHomePage(title: 'תכנון מסלול', itemList: snapshot.data!),
               ),
-              debugShowCheckedModeBanner: false,
-              home: MyHomePage(title: 'תכנון מסלול', itemList: snapshot.data!),
             );
           }
           return LoadingDataScreen();
@@ -94,21 +105,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             body: Center(
-              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                CircularProgressIndicator(),
-                Text(
-                  "טוען נתונים...",
-                  textDirection: TextDirection.rtl,
-                )
-              ]),
-            )));
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        CircularProgressIndicator(),
+        Text(
+          "טוען נתונים...",
+          textDirection: TextDirection.rtl,
+        )
+      ]),
+    )));
   }
 
   Widget ErrorScreen(String error) {
     return MaterialApp(
         home: Scaffold(
             body:
-            Center(child: Text(error, textDirection: TextDirection.ltr))));
+                Center(child: Text(error, textDirection: TextDirection.ltr))));
   }
 }
 
@@ -124,8 +135,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _isAscending = true;
+  int _currentSortColumn = 2;
+  Size _size = Size(1920, 1080);
+
   @override
   Widget build(BuildContext context) {
+    _size = MediaQuery.of(context).size;
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
@@ -138,7 +154,160 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   RoutePlanner() {
-    //todo: this
+    return ItemList();
   }
 
+  ItemList() {
+    return Column(
+      children: [
+        getItem(context, 0),
+        Container(
+          margin: EdgeInsets.only(top: 10),
+          height: _size.height / 3,
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 30,
+                  //Provider.of<ItemListProvider>(context, listen: true).list.length + 1,
+              itemBuilder: (context, idx) => getItem(context, idx+1)),
+        ),
+      ],
+    );
+  }
+
+  getItem(context, idx) {
+    return Card(
+        margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+        elevation: 3,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: (idx == 0) ? HeadersInfo() : ItemInfo(0),//idx - 1),
+          ),
+        ));
+  }
+
+  HeadersInfo() {
+    return Row(children: [
+      SizedBox(width: _size.width / 80),
+      ExpandedSizedTextBox('שם'),
+      ExpandedSizedTextBox('כתובת'),
+      InkWell(
+        child: ExpandedSizedTextBox('שכונה'),
+        onTap: () => sortColumn(sortByNeighbors),
+      ),
+      InkWell(
+          child: ExpandedSizedTextBox('עיר'),
+          onTap: () => sortColumn(sortByCity)),
+      ExpandedSizedTextBox('טלפון'),
+      ExpandedSizedTextBox('תיאור'),
+      ExpandedSizedTextBox('קטגוריה'),
+      InkWell(
+          child: ExpandedSizedTextBox('תאריך'),
+          onTap: () => sortColumn(sortByDate)),
+      ExpandedSizedTextBox('הערות'),
+    ]);
+  }
+
+  ItemInfo(int index) {
+    Item item =
+        Provider.of<ItemListProvider>(context, listen: true).list[index].item2;
+    Map info = {
+      'select': Provider.of<ItemListProvider>(context, listen: true)
+          .list[index]
+          .item1,
+      0: item.name,
+      1: item.address,
+      2: item.neighborhood,
+      3: item.city,
+      4: item.phone,
+      5: item.description,
+      6: item.category,
+      7: item.date,
+      8: item.comments,
+    };
+    List<Widget> textBoxes = [];
+    info.forEach((key, value) {
+      if (value.runtimeType == DateTime) {
+        textBoxes.add(Expanded(
+          child: SizedBox(
+              width: _size.width / 12,
+              height: _size.height / 20,
+              child: Center(
+                child: Text(
+                  formatDate(value),
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
+              )),
+        ));
+      } else if (value.runtimeType == bool) {
+        textBoxes.add(Switch(
+            value: Provider.of<ItemListProvider>(context, listen: true)
+                .list[index]
+                .item1,
+            onChanged: (value) {
+              Provider.of<ItemListProvider>(context, listen: false)
+                  .SelectItemAt(index);
+            }));
+      } else {
+        textBoxes.add(Expanded(
+          child: SizedBox(
+              width: _size.width / 12,
+              height: _size.height / 20,
+              child: Center(
+                child: Tooltip(
+                  textStyle: TextStyle(fontSize: 14),
+                  decoration:
+                      BoxDecoration(color: Color.fromARGB(255, 200, 200, 200)),
+                  message: value,
+                  child: Text(
+                    value,
+                    overflow: TextOverflow.fade,
+                    softWrap: false,
+                  ),
+                ),
+              )),
+        ));
+      }
+    });
+    return Row(children: textBoxes);
+  }
+
+  ExpandedSizedTextBox(text) {
+    return Expanded(
+        child: SizedBox(
+            width: _size.width / 12,
+            height: _size.height / 20,
+            child: Center(
+                child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ))));
+  }
+
+  sortColumn(sort) {
+    _isAscending = !_isAscending;
+    Provider.of<ItemListProvider>(context, listen: false)
+        .Sort(sort, _isAscending);
+  }
+
+  sortByNeighbors(a, b) {
+    return a.neighborhood.compareTo(b.neighborhood);
+  }
+
+  sortByCity(a, b) {
+    return a.city.compareTo(b.city);
+  }
+
+  sortByDate(a, b) {
+    return a.date.compareTo(b.date);
+  }
+
+  String formatDate(DateTime date) {
+    return date.day.toString() +
+        '/' +
+        date.month.toString() +
+        '/' +
+        date.year.toString();
+  }
 }
