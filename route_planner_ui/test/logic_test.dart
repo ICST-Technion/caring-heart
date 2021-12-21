22fd4efb9df6991cc7dd6d143ca26f3f94a0525b
= <=>
2021-12-21 17:08:35 +0200

    Initial testing

--- route_planner_ui/test/logic_test.dart
+++ route_planner_ui/test/logic_test.dart
@@ -2,20 +2,39 @@
 // import 'package:test/test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:route_planner_ui/engine.dart';
-
+import 'package:route_planner_ui/item.dart';
 
 void main(){
 
-  // test("Should return an empty list", () {
-  //   List<MyPoint> result = Logic.routePlanningEngine(List<MyPoint>.empty(), 2);
-  //   assert(result.isEmpty);
-  // });
-
-  test("should do something", () {
-
-  print(Logic.coordinatesOfAddress("pinsker st. 51, haifa").runtimeType);
-  assert(false);
+  List<Item> items = List<Item>.empty();
+  test("Empty List, k is zero", () async {
+    List<Item> res = await Engine.routePlanningEngine(items, 0);
+    assert(res.isEmpty);
   });
 
+  test("Empty List, k is not zero", () async {
+    List<Item> res = await Engine.routePlanningEngine(items, 3);
+    assert(res.isEmpty);
+  });
+
+  // items.add(Item(
+  //     address: "address",
+  //     category: "Couch",
+  //     city: "Haifa",
+  //     comments: "",
+  //     date: DateTime.now(),
+  //     description:"",
+  //     email: "mail@gmail.com",
+  //     isChecked: true,
+  //     isCollected: false,
+  //     name: "Yosef",
+  //     id: "",
+  //     phone: "123"
+  // ));
+
+  // test("One element, k is 1", () async {
+  //   List<Item> res = await Engine.routePlanningEngine(items, 1);
+  //   assert(res.length == 1);
+  // });
 
 }
