46e85d53cf0394b32557ea69729bec88e128181e
danielieberman <48915502+danielieberman@users.noreply.github.com>
2022-01-15 22:46:41 +0200

    Moved selected item list to provider
    see title :)

--- route_planner_ui/lib/item_list_provider.dart
+++ route_planner_ui/lib/item_list_provider.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:item_spec/item_spec.dart';
+import 'package:item_spec/pickup_point.dart';
 import 'package:tuple/tuple.dart';
 
 class ItemListProvider with ChangeNotifier {
-  List<Tuple2<bool, Item>> list;
+  List<Tuple2<bool, Item>> itemList;
+  List<PickupPoint> selectedItems;
 
-  ItemListProvider({required this.list});
+  ItemListProvider({required this.itemList, required this.selectedItems});
 
   void Sort(sortFunc, bool isAscending) {
     isAscending
-        ? list.sort((a, b) => sortFunc(a.item2, b.item2))
-        : list.sort((a, b) => sortFunc(b.item2, a.item2));
+        ? itemList.sort((a, b) => sortFunc(a.item2, b.item2))
+        : itemList.sort((a, b) => sortFunc(b.item2, a.item2));
     notifyListeners();
   }
 
   void SelectItemAt(int index) {
-    list[index] = list[index].withItem1(!list[index].item1);
+    itemList[index] = itemList[index].withItem1(!itemList[index].item1);
+    if (itemList[index].item1) {
+      selectedItems
+          .add(PickupPoint(item: itemList[index].item2, pickupTime: ""));
+    } else {
+      selectedItems.removeWhere((e) => e.item == itemList[index].item2);
+    }
     notifyListeners();
   }
 
   bool isSelectedEmpty() {
-    return list.any((element) => element.item1);
+    return selectedItems.isEmpty;
+  }
+
+  void changePickupTimeAt(idx, time) {
+    selectedItems[idx] =
+        PickupPoint(item: selectedItems[idx].item, pickupTime: time!);
+  }
+
+  List<PickupPoint> getSelectedItems() {
+    return selectedItems;
+  }
+
+  moveSelectedItemAt(prev, next) {
+    final item = selectedItems.removeAt(prev);
+    selectedItems.insert(next, item);
+    notifyListeners();
   }
 }
