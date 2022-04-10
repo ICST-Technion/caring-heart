9a30bbd179e115e5d21465bcee1aeb276d7debfc
Dor-B <dor46200@gmail.com>
2022-04-10 20:07:03 +0300

    add user data to auth service interface

--- nivapp/lib/services/auth_service_i.dart
+++ nivapp/lib/services/auth_service_i.dart
@@ -1,4 +1,7 @@
 abstract class AuthServiceI {
+  String? name;
+  String? uid;
+  String? userEmail;
   Future<bool> isUserRemembered();
   Future<String?> signInWithEmailPassword(String email, String password);
   Future<String> signOut();
