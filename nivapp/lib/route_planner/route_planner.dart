b93a3e277636f3d190ecb0c1c4763a538bcca32e
Daniel Lieberman <daniel.lie@campus.technion.ac.il>
2022-04-11 11:47:40 +0300

    Started Route Planner Refactor

---
+++ nivapp/lib/route_planner/route_planner.dart
@@ -0,0 +1,301 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:nivapp/easy_future_builder.dart';
+import 'package:nivapp/item_spec.dart';
+import 'package:nivapp/main.dart';
+import 'package:nivapp/route_planner/route_planner_provider.dart';
+import 'package:nivapp/services/auth_service_i.dart';
+import 'package:nivapp/services/inventory_service_i.dart';
+import 'package:nivapp/services/routes_service_i.dart';
+import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
+
+class RoutePlanner extends StatelessWidget {
+  const RoutePlanner({Key? key}) : super(key: key);
+
+  InventoryServiceI get inventoryService => injector.get();
+
+  @override
+  Widget build(BuildContext context) {
+    return easyFutureBuilder<List<Item>>(
+        future: inventoryService.getCheckedItems(),
+        //getDay: () => DateTime(2021,12,22)),
+        doneBuilder: (context, List<Item> _itemList) => ChangeNotifierProvider(
+            create: (context) => RoutePlannerProvider(
+                itemList: _itemList.map((e) => Tuple2(false, e)).toList(),
+                selectedItems: []),
+            child: RoutePlannerUI(title: "תכנון מסלול")));
+  }
+}
+
+class RoutePlannerUI extends StatefulWidget {
+  var isLoading = false;
+
+  RoutePlannerUI({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<RoutePlannerUI> createState() => _RoutePlannerUIState();
+}
+
+class _RoutePlannerUIState extends State<RoutePlannerUI> {
+  bool _isAscending = true;
+  DateTime selectedDate = DateTime.now();
+  TextEditingController ctrl = TextEditingController();
+
+  RoutePlannerProvider getProvider(BuildContext _context, bool _listen) =>
+      Provider.of<RoutePlannerProvider>(_context, listen: _listen);
+
+  @override
+  void initState() {
+    ctrl.text = Logic.formatDate(DateTime.now());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Logic.getProvider(context, false).loadNewRoute(selectedDate, notify: false);
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(appBar: MyAppBar(), body: RoutePlanner()),
+    );
+  }
+
+  AppBar MyAppBar() {
+    if (!widget.auth.remember) {
+      return AppBar(title: Text(widget.title));
+    }
+    return AppBar(title: Text(widget.title), actions: [
+      IconButton(
+          onPressed: () async {
+            await widget.auth.signOut();
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => Login(func: (a) => MyApp(auth: a)),
+            ));
+          },
+          icon: Icon(Icons.logout))
+    ]);
+  }
+
+  Widget RoutePlanner() {
+    return SizedBox(
+      width: Logic.ScreenSize(context).width,
+      height: Logic.ScreenSize(context).height,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            tooltip: 'שליחה',
+            child: Icon(Icons.send),
+            onPressed: () {
+              RouteDialogFuncs(context: context, selectedDate: selectedDate)
+                  .ShowRouteDialog();
+            }),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Logic.getProvider(context, true).isLoading
+                ? LinearProgressIndicator(minHeight: 9)
+                : SizedBox(),
+            DateBtn(),
+            ItemList(),
+            Text(
+              'המוצרים שבחרת:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Divider(thickness: 1),
+            SelectedList(context: context).SelectedItemList(true),
+            //MyMap()
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget ItemList() {
+    return Column(
+      children: [
+        Container(
+            child: Card(
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: getItem(context, -1)),
+            margin: EdgeInsets.only(top: 10)),
+        Card(
+          margin: EdgeInsets.all(10),
+          elevation: 4,
+          child: Container(
+            margin: EdgeInsets.only(top: 10),
+            height: Logic.ScreenSize(context).height / 2.6,
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: Logic.getProvider(context, true).itemList.length,
+                itemBuilder: (context, idx) => getItem(context, idx)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getItem(context, idx) {
+    return Card(
+        elevation: 0,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: (idx == -1) ? HeadersInfo() : ItemInfo(idx),
+          ),
+        ));
+  }
+
+  Widget HeadersInfo() {
+    List titles = [
+      'שם',
+      'כתובת',
+      'שכונה',
+      'עיר',
+      'טלפון',
+      'תיאור',
+      'תאריך',
+      'הערות'
+    ];
+    List<Widget> textBoxes = [
+      SizedBox(width: Logic.ScreenSize(context).width / 30)
+    ];
+    titles.forEach((element) {
+      textBoxes.add(Expanded(
+        child: SizedBox(
+            width: Logic.ScreenSize(context).width / 12,
+            height: Logic.ScreenSize(context).height / 20,
+            child: Center(
+              child: Text(
+                element,
+                overflow: TextOverflow.fade,
+                softWrap: false,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            )),
+      ));
+    });
+    return Row(children: textBoxes);
+  }
+
+  Widget ItemInfo(int index) {
+    Item item = Logic.getProvider(context, true).itemList[index].item2;
+    Map info = {
+      0: item.name,
+      1: item.address,
+      2: item.neighborhood,
+      3: item.city,
+      4: item.phone,
+      5: item.description,
+      6: item.date,
+      7: item.comments,
+    };
+    List<Widget> textBoxes = [];
+    info.forEach((key, value) {
+      if (value.runtimeType == DateTime) {
+        textBoxes.add(Expanded(
+          child: SizedBox(
+              width: Logic.ScreenSize(context).width / 12,
+              height: Logic.ScreenSize(context).height / 20,
+              child: Center(
+                child: Text(
+                  Logic.formatDate(value),
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
+              )),
+        ));
+      } else {
+        textBoxes.add(Expanded(
+          child: SizedBox(
+              width: Logic.ScreenSize(context).width / 12,
+              height: Logic.ScreenSize(context).height / 20,
+              child: Center(
+                child: Tooltip(
+                  textStyle: TextStyle(fontSize: 14),
+                  decoration:
+                      BoxDecoration(color: Color.fromARGB(255, 200, 200, 200)),
+                  message: value,
+                  child: Text(
+                    value,
+                    overflow: TextOverflow.fade,
+                    softWrap: false,
+                  ),
+                ),
+              )),
+        ));
+      }
+    });
+    textBoxes.insert(0, Builder(builder: (newContext) {
+      return Switch(
+          value: Logic.getProvider(newContext, true).itemList[index].item1,
+          onChanged: (value) {
+            Logic.getProvider(context, false).SelectItemAt(index);
+          });
+    }));
+    return Row(children: textBoxes);
+  }
+
+  Widget MyMap() {
+    return SizedBox(
+        height: Logic.ScreenSize(context).height / 2,
+        width: Logic.ScreenSize(context).width,
+        child: Center(child: Container()));
+  }
+
+  ExpandedSizedTextBox(text) {
+    return Expanded(child: SizedTextBox(text));
+  }
+
+  Widget SizedTextBox(text) {
+    return SizedBox(
+        width: Logic.ScreenSize(context).width / 12,
+        height: Logic.ScreenSize(context).height / 20,
+        child: Center(
+            child: Text(
+          text,
+          style: TextStyle(fontWeight: FontWeight.bold),
+          overflow: TextOverflow.fade,
+          softWrap: false,
+        )));
+  }
+
+  sortColumn(sort) {
+    _isAscending = !_isAscending;
+    Logic.getProvider(context, false).Sort(sort, _isAscending);
+  }
+
+  Widget DateBtn() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 10),
+        Container(
+            height: Logic.ScreenSize(context).height / 15,
+            width: Logic.ScreenSize(context).width / 10,
+            child: TextFormField(
+                readOnly: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+                controller: ctrl,
+                style: TextStyle(fontSize: 16))),
+        TextButton(
+            onPressed: () async {
+              DateTime? date = await showDatePicker(
+                  context: context,
+                  initialDate: selectedDate,
+                  firstDate: DateTime(2020, 1),
+                  lastDate: DateTime.now().add(Duration(days: 365)));
+              setState(() {
+                selectedDate = date!;
+                ctrl.text = Logic.formatDate(selectedDate);
+                Logic.getProvider(context, false).loadNewRoute(date);
+              });
+            },
+            child: Text('שינוי התאריך')),
+      ],
+    );
+  }
+}
---
+++ nivapp/lib/route_planner/route_planner_provider.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:nivapp/item_spec.dart';
+import 'package:nivapp/pickup_point.dart';
+import 'package:nivapp/services/routes_service_i.dart';
+import 'package:tuple/tuple.dart';
+
+class RoutePlannerProvider with ChangeNotifier {
+  List<Tuple2<bool, Item>> itemList;
+  List<PickupPoint> selectedItems;
+  bool isLoading = false;
+  RoutesServiceI _routeService;
+
+  RoutePlannerProvider(this._routeService,
+      {required this.itemList, required this.selectedItems});
+
+  void Sort(sortFunc, bool isAscending) {
+    isAscending
+        ? itemList.sort((a, b) => sortFunc(a.item2, b.item2))
+        : itemList.sort((a, b) => sortFunc(b.item2, a.item2));
+    notifyListeners();
+  }
+
+  void SelectItemAt(int index) {
+    itemList[index] = itemList[index].withItem1(!itemList[index].item1);
+    if (itemList[index].item1) {
+      selectedItems
+          .add(PickupPoint(item: itemList[index].item2, pickupTime: ""));
+    } else {
+      selectedItems.removeWhere((e) => e.item.id == itemList[index].item2.id);
+    }
+    notifyListeners();
+  }
+
+  bool isSelectedEmpty() {
+    return selectedItems.isEmpty;
+  }
+
+  bool isThereEmptyPickupTime() {
+    return selectedItems.any((element) => element.pickupTime == '');
+  }
+
+  void loadNewRoute(DateTime date, {bool notify = true}) async {
+    isLoading = true;
+    if (notify) {
+      notifyListeners();
+    }
+    selectedItems = await _routeService.getItems(getDay: () => date);
+    List<String> tempItemList = selectedItems.map((e) => e.item.id).toList();
+    itemList = itemList
+        .map((e) => (tempItemList.contains(e.item2.id))
+            ? Tuple2(true, e.item2)
+            : Tuple2(false, e.item2))
+        .toList();
+    isLoading = false;
+    if (notify) {
+      notifyListeners();
+    }
+  }
+
+  void changePickupTimeAt(idx, time) {
+    selectedItems[idx] =
+        PickupPoint(item: selectedItems[idx].item, pickupTime: time!);
+  }
+
+  List<PickupPoint> getSelectedItems() {
+    return selectedItems;
+  }
+
+  moveSelectedItemAt(prev, next) {
+    final item = selectedItems.removeAt(prev);
+    selectedItems.insert(next, item);
+    notifyListeners();
+  }
+}
