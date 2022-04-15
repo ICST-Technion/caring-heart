bb35fb85813ebfdefa1c46841c3c231f0d44e5c8
Dor-B <dor46200@gmail.com>
2022-04-15 17:58:48 +0300

    Fix mockito error: change reference of default parameter  function in RoutesServiceI.getItems(getDay)

--- nivapp/lib/services/routes_service_i.dart
+++ nivapp/lib/services/routes_service_i.dart
@@ -1,10 +1,11 @@
 import 'package:nivapp/pickup_point.dart';
 
+DateTime today() => DateTime.now();
+
 abstract class RoutesServiceI {
   /// Returns the items in current day's route.
   /// new name option: getDailyRoute.
-  Future<List<PickupPoint>> getItems(
-      {DateTime Function() getDay = DateTime.now});
+  Future<List<PickupPoint>> getItems({DateTime Function() getDay = today});
 
   /// add route to firebase routes database from PickupPoint list.
   Future<void> addRouteByItemList(List<PickupPoint> list, DateTime date);
