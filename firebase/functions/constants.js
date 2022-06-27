298a251ee995820825a136f36f865e2784ab213f
Dor-B <dor46200@gmail.com>
2022-06-28 00:02:22 +0300

    cloudfunctions from george

--- firebase/functions/constants.js
+++ firebase/functions/constants.js
@@ -1,78 +1,52 @@
 exports.constants = {
-    KEY_COL_IDX: 1,
-    FIRST_COL_IDX: 2,
-    // LAST_COL_IDX: 14,
+  SPREADSHEET_IDS: {
+    TEST: '1X1q6YAQdXoEmkUn2KhygDzswDlwZ8epW2NL3jSu1tj0',
+    PRODUCTION: '10b4vqS0cbBlqLuDGgSyOljJaCziwTYyHfReQq6jdRdg',
+  },
+  COLLECTIONS: {
+    TEST: {
+      INVENTORY: 'inventoryTest',
+      REPORTS: 'reportsTest',
+    },
+    PRODUCTION: {
+      INVENTORY: 'inventory',
+      REPORTS: 'reports',
+    }
+  },
   
-    // CHANGED
-    LAST_COL_IDX: 15,
-    
-    FIELDS: [
-      // {name: "name", required: true, checkForEmptyness: true},
-      // {name: "address", required: true, checkForEmptyness: true},
-      // {name: "neighborhood", required: true, checkForEmptyness: true},
-      // {name: "city", required: true, checkForEmptyness: true},
-      // {name: "phone", required: true, checkForEmptyness: true},
-      // {name: "category", required: true, checkForEmptyness: true},
-      // {name: "description", required: true, checkForEmptyness: true},
-      // {name: "date", required: true, checkForEmptyness: true},
-      // {name: "comments", required: false, checkForEmptyness: true},
-      // {name: "email", required: true, checkForEmptyness: true},
-      // {name: "isChecked", required: true, checkForEmptyness: false},
-      // {name: "isCollected", required: true, checkForEmptyness: false},
-  
-      {name: "name", required: true, checkForEmptyness: true},
-      {name: "address", required: true, checkForEmptyness: true},
-  
-      // NEW
-      {name: "floor", required: true, checkForEmptyness: true},
-      // NEW
-      {name: "apartment", required: true, checkForEmptyness: true},
-  
-      {name: "neighborhood", required: true, checkForEmptyness: true},
-      {name: "city", required: true, checkForEmptyness: true},
-      {name: "phone", required: true, checkForEmptyness: true},
-  
-      // DELETED
-      // {name: "category", required: true, checkForEmptyness: true},
-  
-      {name: "description", required: true, checkForEmptyness: true},
-      {name: "date", required: true, checkForEmptyness: true},
-      {name: "comments", required: false, checkForEmptyness: true},
-      {name: "email", required: true, checkForEmptyness: true},
-      {name: "isChecked", required: true, checkForEmptyness: false},
-      {name: "isCollected", required: true, checkForEmptyness: false},
-    ],
-    FORM_FIELDS: [
-      // "date", 
-      // "email", 
-      // "name", 
-      // "city",
-      // "neighborhood", 
-      // "address",
-      // "phone", 
-      // "category", 
-      // "description", 
-      // "comments",
-  
-      "date", 
-      "email", 
-      "name", 
-      "city",
-      "neighborhood", 
-      "address",
-  
-      // NEW
-      "floor",
-      "apartment",
-  
-      "phone", 
-  
-      // DELETED
-      // "category", 
-  
-      "description", 
-      "comments",
-    ]
-  
-  }
-  
+  KEY_COL_IDX: 1,
+  FIRST_COL_IDX: 2,
+  LAST_COL_IDX: 15,
+  LAST_COL_LETTER: "O",
+  DATE_IDX:10,
+  Default_Sheet: "Sheet1",
+
+  FIELDS: [
+    {name: "name", required: true, checkForEmptyness: true},
+    {name: "address", required: true, checkForEmptyness: true},
+    {name: "floor", required: true, checkForEmptyness: true},
+    {name: "apartment", required: true, checkForEmptyness: true},
+    {name: "neighborhood", required: false, checkForEmptyness: true},
+    {name: "city", required: true, checkForEmptyness: true},
+    {name: "phone", required: true, checkForEmptyness: true},
+    {name: "description", required: true, checkForEmptyness: true},
+    {name: "date", required: true, checkForEmptyness: true},
+    {name: "comments", required: false, checkForEmptyness: true},
+    {name: "email", required: false, checkForEmptyness: true},
+    {name: "isChecked", required: true, checkForEmptyness: false},
+    {name: "isCollected", required: true, checkForEmptyness: false},
+  ],
+  FORM_FIELDS: [
+    "date", 
+    "email", 
+    "name", 
+    "city",
+    "neighborhood", 
+    "address",
+    "phone", 
+    "description", 
+    "comments",
+    "floor",
+    "apartment",
+  ]
+}
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -14,12 +14,6 @@ function format(field, value, format_fields=FORMAT_FIELDS){
     return value;
 }
 
-//const spreadsheetId = "1KEnhldGfYakoIgibePV0pxbEiEr2kgRbj2RQyg3R51c";
-const spreadsheetId = "1X1q6YAQdXoEmkUn2KhygDzswDlwZ8epW2NL3jSu1tj0";
-//const spreadsheetId = "1TC2SMsINKsXbUpqb75Bytl9B2VV3O82Yq66Q0hhP-uk";
-
-const inventoryCollection = 'inventoryTest';
-const reportsCollection = 'reportsTest';
 const docPath = collection => `/${collection}/{documentId}`;
 
 
@@ -36,16 +30,26 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.createSheets = functions.firestore.document(docPath(inventoryCollection))
-    .onCreate((snap, context) => {
-        const data = snap.data();
-        data.fromSheets = data.fromSheets ?? false;
-        if (data.fromSheets){
-            return snap.ref.set({ fromSheets: false}, {merge: true});
-        }
-        const values = getRowValues(data, context.params.documentId);
-        return sheetAppendLine(values, getSheetName());
-    });
+function newSheetLineFromFirebase(spreadsheetId, snap, context){
+    const data = snap.data();
+    data.fromSheets = data.fromSheets ?? false;
+    
+    if (data.fromSheets){
+        return snap.ref.set({ fromSheets: false}, {merge: true});
+    }
+    const values = getRowValues(data, context.params.documentId);
+    let sheetName = getSheetName(values[0][constants.DATE_IDX]);
+    if (!isSheetExists(spreadsheetId, sheetName)){
+        sheetName = constants.Default_Sheet;
+    }
+    return sheetAppendLine(spreadsheetId, values, sheetName);
+}
+
+exports.createSheets = functions.firestore.document(docPath(constants.COLLECTIONS.TEST.INVENTORY))
+    .onCreate((snap, context) => newSheetLineFromFirebase(constants.SPREADSHEET_IDS.TEST, snap, context));
+
+exports.createSheets = functions.firestore.document(docPath(constants.COLLECTIONS.PRODUCTION.INVENTORY))
+    .onCreate((snap, context) => newSheetLineFromFirebase(constants.SPREADSHEET_IDS.PRODUCTION, snap, context));
 
 function getRowValues(data, documentId) {
     const columnData = constants.FIELDS.map(field => format(field.name, data[field.name]) ?? '');
@@ -53,52 +57,64 @@ function getRowValues(data, documentId) {
     return values;
 }
 
-async function sheetAppendLine(values, range) {
-    await jwtAuthPromise;
-    return sheets.spreadsheets.values.append(
-        {
-            auth: jwtClient,
-            spreadsheetId: spreadsheetId,
-            range: range,
-            valueInputOption: "USER_ENTERED",
-            insertDataOption: 'INSERT_ROWS',
-            resource: {
-                values: values,
-            },
-        },
-        {}
-    );
-}
+function between(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+  }
 
-exports.updateSheets = functions.firestore.document(docPath(inventoryCollection))
-    .onUpdate(async (change, context) => {
-        const data = change.after.data();
-        functions.logger.log("RUNNING updateSheets() with new data", data);
-        if (data.fromSheets){
-            functions.logger.log("Change is from sheets");
-            return change.after.ref.set({ fromSheets: false}, {merge: true});
-        }
-        functions.logger.log("Change is not from sheets");
-        if (change.before.data().fromSheets && !data.fromSheets){
-            functions.logger.log("Change was prev. from sheets, now it's not");
-            return null;
-        }
-        let formula = `=MATCH("${context.params.documentId}", ${getSheetName()}!A:A, 0)`;
-        sheetsUpdate([[formula]], "LookUp_Sheet789!A1", "USER_ENTERED").then(( response) => {
+async function sheetAppendLine(spreadsheetId, values, sheetName) {
+    const row = between(2,850)
+    const col = String.fromCharCode(between(65,90))    
+    sheetsUpdate(spreadsheetId, [[""]], `LookUp_Sheet789!${col}${row}`, "USER_ENTERED").then(( _) => {
+    let formula = `=lastValue("${sheetName}")`;
+        sheetsUpdate(spreadsheetId, [[formula]], `LookUp_Sheet789!${col}${row}`, "USER_ENTERED").then(( response) => {
             const line = response.data.updatedData.values[0][0];
-            functions.logger.log(`Found line ${line}`);
-            const values = getRowValues(data, context.params.documentId);
-            functions.logger.log(`Col values are`, values);
-            if (isNaN(line)){
-                sheetAppendLine(values, getSheetName());
-            }
-            else{
-                sheetsUpdate(values, `${getSheetName()}!A${line}:N${line}`, "USER_ENTERED");
-            }
+            functions.logger.log(`Found line ${line}`);            
+            sheetsUpdate(spreadsheetId, values, `${sheetName}!A${line}:${constants.LAST_COL_LETTER}${line}`, "USER_ENTERED");            
+            
         });
     });
+}
 
-async function sheetsUpdate(values, range, valueInputOption) {
+async function updateSheetLineFromFirebase(spreadsheetId, change, context){
+    const data = change.after.data();
+    functions.logger.log("RUNNING updateSheets() with new data", data);
+    if (data.fromSheets){
+        functions.logger.log("Change is from sheets");
+        return change.after.ref.set({ fromSheets: false}, {merge: true});
+    }
+    functions.logger.log("Change is not from sheets");
+    if (change.before.data().fromSheets && !data.fromSheets){
+        functions.logger.log("Change was prev. from sheets, now it's not");
+        return null;
+    }
+    let sheetName = getSheetName(formatDate(data.date));
+    if (!isSheetExists(spreadsheetId, sheetName)){
+        sheetName = constants.Default_Sheet;
+    }
+    let formula = `=MATCH("${context.params.documentId}", '${sheetName}'!A:A, 0)`;
+    sheetsUpdate(spreadsheetId, [[formula]], "LookUp_Sheet789!A1", "USER_ENTERED").then(( response) => {
+        const line = response.data.updatedData.values[0][0];
+        functions.logger.log(`Found line ${line}`);
+        const values = getRowValues(data, context.params.documentId);
+        functions.logger.log(`Col values are`, values);
+        if (isNaN(line)){
+            sheetAppendLine(spreadsheetId, values, sheetName);
+        }
+        else{
+            sheetsUpdate(spreadsheetId, values, `${sheetName}!A${line}:${constants.LAST_COL_LETTER}${line}`, "USER_ENTERED");
+        }
+    });
+}
+
+exports.updateSheets = functions.firestore.document(docPath(constants.COLLECTIONS.TEST.INVENTORY))
+    .onUpdate((change, context) => updateSheetLineFromFirebase(constants.SPREADSHEET_IDS.TEST, change, context));
+
+exports.updateSheets = functions.firestore.document(docPath(constants.COLLECTIONS.PRODUCTION.INVENTORY))
+    .onUpdate((change, context) => updateSheetLineFromFirebase(constants.SPREADSHEET_IDS.PRODUCTION, change, context));
+
+async function sheetsUpdate(spreadsheetId, values, range, valueInputOption) {
     await jwtAuthPromise;
     return sheets.spreadsheets.values.update({
         auth: jwtClient,
@@ -119,22 +135,57 @@ function formatDate(timestamp)
     if (typeof timestamp === 'string' || timestamp instanceof String) {
         return timestamp;
     }
-    date = timestamp.toDate();
+    const date = timestamp.toDate();
     return `${date.getUTCDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 }
 
-function getSheetName() {
-    return "Sheet1";
+async function isSheetExists(spreadsheetId, sheetName)
+{
+    let formula = `=ISREF('${sheetName}'!A1)`;
+    await sheetsUpdate(spreadsheetId, [[formula]], "LookUp_Sheet789!A3", "USER_ENTERED", spreadsheetId).then(( response) => {
+        const result = response.data.updatedData.values[0][0];
+        return result.toLowerCase().startsWith("true");
+
+    }, (reason)=> {
+        return false;
+    });
+}
+
+function getSheetName(dateField) {
+    try{
+        const arr = dateField.split("/");
+        if (!arr || !arr.length || arr.length !=3){
+            return constants.Default_Sheet;
+        }
+        const month = parseInt(arr[1]);
+        const year = parseInt(arr[2]) -2000;
+        if (month <1 || month >12 || year <0){
+            return constants.Default_Sheet;
+        }
+        return `${month}/${year}`;
+
+    }
+    catch{
+        return constants.Default_Sheet;
+    }
 }
 
 function isVisited(reportData){
     return ['collected', 'canceled'].includes(reportData?.status); 
 }
 
-exports.updateInventoryOnReport = functions.firestore.document(docPath(reportsCollection)).onWrite(async (change, context) => {
+async function updateIsCollectedOnReport (inventoryCollection, change, context) {
     const isReportCollected = isVisited(change.after.data());
     const reportId = context.params.documentId;
     const inventoryItemId = reportId;  // They are the same
     await db.doc(`/${inventoryCollection}/${inventoryItemId}`)
             .set({isCollected: isReportCollected}, { merge: true });
-})
+}
+
+exports.updateInventoryOnReport = functions.firestore.document(docPath(constants.COLLECTIONS.TEST.REPORTS)).onWrite((change, context) => 
+    updateIsCollectedOnReport(constants.COLLECTIONS.TEST.INVENTORY, change, context)
+)
+
+exports.updateInventoryOnReport = functions.firestore.document(docPath(constants.COLLECTIONS.PRODUCTION.REPORTS)).onWrite((change, context) => 
+    updateIsCollectedOnReport(constants.COLLECTIONS.PRODUCTION.INVENTORY, change, context)
+)
